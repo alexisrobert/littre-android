d52e3dfc75c2d661ead0693c70678b943bf6cbf4
alexis.robert <alexis.robert@localhost>
2009-05-01 14:23:11 +0000

    Add GPL header

--- client/src/org/alexis/libstardict/IndexDB.java
+++ client/src/org/alexis/libstardict/IndexDB.java
@@ -1,3 +1,20 @@
+/*
+ * Stardict """library""" for Android (not so UI-independent :) )
+ * Copyright (C) 2009 Alexis ROBERT <alexis.robert@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, at version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.alexis.libstardict;
 
 import android.content.Context;
@@ -7,7 +24,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class IndexDB extends SQLiteOpenHelper {
 	private boolean tofill = false;
-	private static int HISTORY_DEPTH = 50;
+	private static int HISTORY_DEPTH = 50; // TODO: Make this configurable
 	
 	public IndexDB(Context context) {
 		super(context, "index.db", null, 1);
