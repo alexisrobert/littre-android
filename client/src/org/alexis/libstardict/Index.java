cf7924604806cd52c0449e272a5146189c23186c
alexis.robert <alexis.robert@localhost>
2009-12-21 18:25:41 +0000

    Updating copyright notices in all source files

--- client/src/org/alexis/libstardict/Index.java
+++ client/src/org/alexis/libstardict/Index.java
@@ -1,5 +1,5 @@
 /*
- * Stardict """library""" for Android (not so UI-independent :) )
+ * Littre dictionnary for Android
  * Copyright (C) 2009 Alexis ROBERT <alexis.robert@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
